757be638f1641e1ae0df1046e3af986d9cfe81f4
Iggy Glassman <Iggy.Glassman@GMR.local>
2021-01-10 20:29:19 -0800

    did some things

---
+++ TextJustifyer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextJustifyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "This is a test paragraph used for testing this text justification thingy.\nI am unsure where I am going with my current musings,\nbut this is probably enough of a sample if I were to wrap it up here, which is exactly what I am doing currently.";
+
+            while (true)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                // todo: justify the thing
+            }
+        }
+    }
+}
---
+++ TextJustifyer/RopeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextJustifyer
+{
+    public class RopeNode
+    {
+        public RopeNode Left;
+        public RopeNode Right;
+
+        public int Weight;
+        public string Text;
+    }
+}
